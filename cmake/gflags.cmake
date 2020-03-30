451e4d9003ce57eda99a0ece08571b3c432e4d32
unknown <unknown@gmail.com>
2020-03-30 19:53:49 +0800

    MOD move add_dependencies to third party cmake file; ADD example for add_custom_target and add_custom_command

--- cmake/gflags.cmake
+++ cmake/gflags.cmake
@@ -1,22 +1,25 @@
 include(ExternalProject)
+set(PROJ_GFLAGS external_gflags)
 
-set(GFLAG_ROOT          ${CMAKE_BINARY_DIR}/third-party/gflag-2.2.2)
+set(GFLAG_ROOT          ${CMAKE_BINARY_DIR}/third-party/gflags)
 set(GFLAG_LIB_DIR       ${GFLAG_ROOT}/lib)
 set(GFLAG_INCLUDE_DIR   ${GFLAG_ROOT}/include)
 
 set(GFLAG_URL           https://github.com/gflags/gflags/archive/v2.2.2.zip)
 set(GFLAG_DOWNLOAD_CMD  wget ${GFLAG_URL})
-set(GFLAG_CONFIGURE     cd ${GFLAG_ROOT}/src/gflag-2.2.2 && cmake -D CMAKE_INSTALL_PREFIX=${GFLAG_ROOT} .)
-set(GFLAG_MAKE          cd ${GFLAG_ROOT}/src/gflag-2.2.2 && make)
-set(GFLAG_INSTALL       cd ${GFLAG_ROOT}/src/gflag-2.2.2 && make install)
+set(GFLAG_CONFIGURE     cd ${GFLAG_ROOT}/src/${PROJ_GFLAGS} && cmake -D CMAKE_INSTALL_PREFIX=${GFLAG_ROOT} .)
+set(GFLAG_MAKE          cd ${GFLAG_ROOT}/src/${PROJ_GFLAGS} && make)
+set(GFLAG_INSTALL       cd ${GFLAG_ROOT}/src/${PROJ_GFLAGS} && make install)
 
-ExternalProject_Add(gflag-2.2.2
+ExternalProject_Add(${PROJ_GFLAGS}
         URL                   ${GFLAG_URL}
         #DOWNLOAD_COMMAND      ${GFLAG_DOWNLOAD_CMD}
-        DOWNLOAD_NAME         gflag-2.2.2.zip
+        DOWNLOAD_NAME         gflags-2.2.2.zip
         PREFIX                ${GFLAG_ROOT}
         CONFIGURE_COMMAND     ${GFLAG_CONFIGURE}
         BUILD_COMMAND         ${GFLAG_MAKE}
         INSTALL_COMMAND       ${GFLAG_INSTALL}
 )
 
+# build ${PROJ_GFLAGS} before ${EXECUTABLE_NAME}
+add_dependencies(${EXECUTABLE_NAME} ${PROJ_GFLAGS})
