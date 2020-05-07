7118da2df874442058bdbdd2b93297cecf16cc0b
unknown <unknown@gmail.com>
2020-05-07 12:33:37 +0000

    ADD coverage test

--- test/raw_test.cpp
+++ test/raw_test.cpp
@@ -6,6 +6,10 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    if(argc < 3) {
+        cout << "Usage: ./raw_test <number1> <number2>" << endl;
+        return 1;
+    }
     int i1 = atoi(argv[1]);
     int i2 = atoi(argv[2]);
     cout << add(i1, i2) << endl;
