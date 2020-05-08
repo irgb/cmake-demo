23b57859a9cee8ea439946f9a0ee521c9e9e4665
unknown <unknown@gmail.com>
2020-05-08 09:52:17 +0000

    add gcov support

--- cmake/init.cmake
+++ cmake/init.cmake
@@ -29,23 +29,23 @@ set(CMAKE_CUDA_FLAGS_MINSIZEREL "-O1 -DNDEBUG")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--gc-sections")
 
 # check if cxx compiler support c++11
-include(CheckCXXCompilerFlag)
-CHECK_CXX_COMPILER_FLAG("-std=c++11" COMPILER_SUPPORTS_CXX11)
-
-# set CMAKE_CXX_FLAGS
-if(COMPILER_SUPPORTS_CXX11)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-else()
-    message(FATAL_ERROR "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support.")
-endif()
-
-# set CMAKE_CUDA_FLAGS
-if(COMPILER_SUPPORTS_CXX11)
-    set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -std=c++11")
-else()
-    message(FATAL_ERROR "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support.")
-endif()
-set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -w")
+#include(CheckCXXCompilerFlag)
+#CHECK_CXX_COMPILER_FLAG("-std=c++11" COMPILER_SUPPORTS_CXX11)
+#
+## set CMAKE_CXX_FLAGS
+#if(COMPILER_SUPPORTS_CXX11)
+#    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+#else()
+#    message(FATAL_ERROR "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support.")
+#endif()
+#
+## set CMAKE_CUDA_FLAGS
+#if(COMPILER_SUPPORTS_CXX11)
+#    set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -std=c++11")
+#else()
+#    message(FATAL_ERROR "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support.")
+#endif()
+#set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -w")
 
 # add compiler option to CXX Compiler
 add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:-fno-omit-frame-pointer>")
@@ -53,6 +53,10 @@ add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:-Wno-unused-parameter>")
 add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:-Wno-unused-function>")
 add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:-fdata-sections>")
 add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:-ffunction-sections>")
+add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:-g>")
+add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:-O0>")
+add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:-fprofile-arcs>")
+add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:-ftest-coverage>")
 
 # add compiler option to CUDA Compiler
 add_compile_options("$<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=-fno-omit-frame-pointer>")
@@ -60,5 +64,6 @@ add_compile_options("$<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=-Wno-unused-parameter
 add_compile_options("$<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=-Wno-unused-function>")
 add_compile_options("$<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=-fdata-sections>")
 add_compile_options("$<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=-ffunction-sections>")
-
+add_compile_options("$<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=-fprofile-arcs>")
+add_compile_options("$<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=-ftest-coverage>")
 
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 
 #include "gflags/gflags.h"
 
