66a90aeaa7910393759710f4c478b53f187174af
irgb <4240035+irgb@users.noreply.github.com>
2020-05-09 11:52:51 +0800

    MOD quiet lcov command

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ cd ..
 COVERAGE_FILE=coverage.info
 REPORT_FOLDER=coverage_report
 CUR_DIR=$(pwd)
-lcov --rc lcov_branch_coverage=1 -c -d build -o ${COVERAGE_FILE}_tmp
-lcov --rc lcov_branch_coverage=1  -e ${COVERAGE_FILE}_tmp "*${CUR_DIR}*" -o ${COVERAGE_FILE}
+lcov --rc lcov_branch_coverage=1 -c -d build -o ${COVERAGE_FILE}_tmp -q
+lcov --rc lcov_branch_coverage=1  -e ${COVERAGE_FILE}_tmp "*${CUR_DIR}*" -o ${COVERAGE_FILE} -q
 genhtml --rc genhtml_branch_coverage=1 ${COVERAGE_FILE} -o ${REPORT_FOLDER}
 rm -rf ${COVERAGE_FILE}_tmp
 rm -rf ${COVERAGE_FILE}
